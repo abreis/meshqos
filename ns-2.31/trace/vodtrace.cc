7d2327833c7a3f2d50f59208ab4d67b0d7f957be
Andre Reis <andrebragareis@gmail.com>
2009-02-19 14:39:17 +0000

    VODTrafficTrace: first revision, new objects: VODTraceFile, Application/Traffic/VODTrace; extra 16-bit field for frame type information; licensing

--- ns-2.31/trace/vodtrace.cc
+++ ns-2.31/trace/vodtrace.cc
@@ -1,7 +1,8 @@
-/* -*-	Mode:C++; c-basic-offset:8; tab-width:8; indent-tabs-mode:t -*- */
-/*
+/* Copyright (c) 2009 Andre Braga Reis
+ * andrebragareis at gmail dot com
+ *
  * Copyright (c) Xerox Corporation 1997. All rights reserved.
- *  
+ *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
  * Free Software Foundation; either version 2 of the License, or (at your
@@ -39,18 +40,12 @@
  * which carries forward this exception.
  */
 
-#ifndef lint
-static const char rcsid[] =
-    "@(#) $Header: /cvsroot/nsnam/ns-2/trace/traffictrace.cc,v 1.16 2005/08/26 05:05:31 tomh Exp $ (Xerox)";
-#endif
-
-/* XXX: have not dealt with errors.  e.g., if something fails during
- * TraceFile::setup(), or TrafficTrace is not pointing to a TraceFile,
- * no guarantee about results.
+/*
+ * description
  */
- 
+
 #include <sys/types.h>
-#include <sys/stat.h> 
+#include <sys/stat.h>
 #include <stdio.h>
 
 #include "config.h"
@@ -59,74 +54,66 @@ static const char rcsid[] =
 #endif /* HAVE_NETINET_IN_H */
 
 #include "random.h"
-/* module to implement a traffic generator in ns driven by a trace
- * file.  records in the trace file consist of 2 32 bit fields, the
- * first indicating the inter-packet time in microseconds, and the
- * second indicating the packet length in bytes.  multiple TraffficTrace
- * objects can use the same trace file, and different TrafficTrace
- * objects can use different trace files.  For each TrafficTrace, a
- * random starting point within the trace file is selected.
- */
-
 #include "object.h"
 #include "trafgen.h"
 
+/* TODO: deal with errors
+ */
+
 struct tracerec {
-        u_int32_t trec_time; /* inter-packet time (usec) */
-	u_int32_t trec_size; /* size of packet (bytes */
+	u_int32_t trec_time; // inter-packet time (usec)
+	u_int32_t trec_size; // size of packet (bytes)
+	u_int16_t trec_ftype;	// frame type {I,P,B} (char)
+							// due to the lack of an htonX() for uint8, we 'waste' 16-bits
 };
 
-
-/* object to hold a single trace file */
-
-class TraceFile : public NsObject {
+// object to hold a single trace file
+class VODTraceFile : public NsObject {
  public:
-	TraceFile();
-	void get_next(int&, struct tracerec&); /* called by TrafficGenerator
-						* to get next record in trace.
-						*/
-	virtual int setup();  /* initialize the trace file */
+	VODTraceFile();
+	void get_next(int&, struct tracerec&); // called by TrafficGenerator to get next record in trace
+	virtual int setup();  // initialize the trace file
 	int command(int argc, const char*const* argv);
  protected:
-	void recv(Packet*, Handler*); /* must be defined for NsObject */
-        int status_; 
-	char *name_;  /* name of the file in which the trace is stored */
-	int nrec_;    /* number of records in the trace file */
-	struct tracerec *trace_; /* array holding the trace */
+	void recv(Packet*, Handler*); // must be defined for NsObject
+	int status_;	//
+	char *name_;  // name of the file in which the trace is stored
+	int nrec_;    // number of records in the trace file
+	struct tracerec *trace_; // array holding the trace
 };
 
-/* instance of a traffic generator.  has a pointer to the TraceFile
+/* Instance of a traffic generator. Has a pointer to the VODTraceFile
  * object and implements the interval() function.
  */
 
-class TrafficTrace : public TrafficGenerator {
+class VODTrafficTrace : public TrafficGenerator {
  public:
-	TrafficTrace();
+	VODTrafficTrace();
 	int command(int argc, const char*const* argv);
 	virtual double next_interval(int &);
  protected:
 	virtual void timeout();
-	TraceFile *tfile_;
+	VODTraceFile *tfile_;
 	struct tracerec trec_;
 	int ndx_;
 	void init();
 };
 
-/**************************************************************/
+// [VODTracefile] Tcl handlers
 
-static class TraceFileClass : public TclClass {
+static class VODTraceFileClass : public TclClass {
  public:
-	TraceFileClass() : TclClass("Tracefile") {}
+	VODTraceFileClass() : TclClass("VODTracefile") {}
 	TclObject* create(int, const char*const*) {
-		return (new TraceFile());
+		return (new VODTraceFile());
 	}
-} class_tracefile;
+} class_vodtracefile;
 
-TraceFile::TraceFile() : status_(0)
+VODTraceFile::VODTraceFile() : status_(0)
 {
 }
 
-int TraceFile::command(int argc, const char*const* argv)
+int VODTraceFile::command(int argc, const char*const* argv)
 {
 
 	if (argc == 3) {
@@ -139,26 +126,24 @@ int TraceFile::command(int argc, const char*const* argv)
 	return (NsObject::command(argc, argv));
 }
 
-void TraceFile::get_next(int& ndx, struct tracerec& t)
+void VODTraceFile::get_next(int& ndx, struct tracerec& t)
 {
 	t.trec_time = trace_[ndx].trec_time;
 	t.trec_size = trace_[ndx].trec_size;
+	t.trec_ftype = trace_[ndx].trec_ftype;
 
 	if (++ndx == nrec_)
 		ndx = 0;
-
 }
 
-int TraceFile::setup()
+int VODTraceFile::setup()
 {
 	tracerec* t;
 	struct stat buf;
 	int i;
 	FILE *fp;
 
-	/* only open/read the file once (could be shared by multiple
-	 * SourceModel's
-	 */
+	// only open/read the file once (could be shared by multiple SourceModels
 	if (! status_) {
 		status_ = 1;
 
@@ -167,12 +152,10 @@ int TraceFile::setup()
 			return -1;
 		}
 
+		// get the number of records in the file by the number of bytes in it
 		nrec_ = buf.st_size/sizeof(tracerec);
-		unsigned nrecplus = nrec_ * sizeof(tracerec);
-		unsigned bufst = buf.st_size;
 
-		//	if ((unsigned)(nrec_ * sizeof(tracerec)) != buf.st_size) {
-		if (nrecplus != bufst) {
+		if ((unsigned)(nrec_ * sizeof(tracerec)) != buf.st_size) {
 			printf("bad file size in %s\n", name_);
 			return -1;
 		}
@@ -190,9 +173,9 @@ int TraceFile::setup()
 				return -1 ;
 			}
 			else {
-		
 				t->trec_time = ntohl(t->trec_time);
 				t->trec_size = ntohl(t->trec_size);
+				t->trec_ftype = ntohs(t->trec_ftype);
 			}
 
 	}
@@ -202,40 +185,40 @@ int TraceFile::setup()
 
 }
 
-void TraceFile::recv(Packet*, Handler*)
+void VODTraceFile::recv(Packet*, Handler*)
 {
         /* shouldn't get here */
         abort();
 }
 
-/**************************************************************/
+// [Application/Traffic/VODTrace] Tcl handlers
 
-static class TrafficTraceClass : public TclClass {
+static class VODTrafficTraceClass : public TclClass {
  public:
-	TrafficTraceClass() : TclClass("Application/Traffic/Trace") {}
+	VODTrafficTraceClass() : TclClass("Application/Traffic/VODTrace") {}
 	TclObject* create(int, const char*const*) {
-	        return(new TrafficTrace());
+	        return(new VODTrafficTrace());
 	}
 } class_traffictrace;
 
-TrafficTrace::TrafficTrace()
+VODTrafficTrace::VODTrafficTrace()
 {
-	tfile_ = (TraceFile *)NULL;
+	tfile_ = (VODTraceFile *)NULL;
 }
 
-void TrafficTrace::init()
+void VODTrafficTrace::init()
 {
-	if (tfile_) 
+	if (tfile_)
 		ndx_ = tfile_->setup();
 }
 
-int TrafficTrace::command(int argc, const char*const* argv)
+int VODTrafficTrace::command(int argc, const char*const* argv)
 {
 	Tcl& tcl = Tcl::instance();
-	
+
 	if (argc == 3) {
 		if (strcmp(argv[1], "attach-tracefile") == 0) {
-			tfile_ = (TraceFile *)TclObject::lookup(argv[2]);
+			tfile_ = (VODTraceFile *)TclObject::lookup(argv[2]);
 			if (tfile_ == 0) {
 				tcl.resultf("no such node %s", argv[2]);
 				return(TCL_ERROR);
@@ -243,31 +226,29 @@ int TrafficTrace::command(int argc, const char*const* argv)
 			return(TCL_OK);
 		}
 	}
-
 	return (TrafficGenerator::command(argc, argv));
 
 }
 
-void TrafficTrace::timeout()
+void VODTrafficTrace::timeout()
 {
-        if (! running_)
-                return;
+	if (! running_)
+		return;
 
-        /* send a packet */
-	// Note:  May need to set "NEW_BURST" flag in sendmsg() for 
-	// signifying a new talkspurt when using vat traces.
-	// (see expoo.cc, tcl/ex/test-rcvr.tcl)
+	// send a packet
+	/* Note:  May need to set "NEW_BURST" flag in sendmsg() for signifying
+	 * a new talkspurt when using vat traces(see expoo.cc, tcl/ex/test-rcvr.tcl)
+	 */
 	agent_->sendmsg(size_);
-        /* figure out when to send the next one */
-        nextPkttime_ = next_interval(size_);
-        /* schedule it */
-        timer_.resched(nextPkttime_);
+	// figure out when to send the next one
+	nextPkttime_ = next_interval(size_);
+	// schedule it */
+	timer_.resched(nextPkttime_);
 }
 
-
-double TrafficTrace::next_interval(int& size)
+double VODTrafficTrace::next_interval(int& size)
 {
-        tfile_->get_next(ndx_, trec_);
+	tfile_->get_next(ndx_, trec_);
 	size = trec_.trec_size;
 	return(((double)trec_.trec_time)/1000000.0); /* usecs->secs */
 }
